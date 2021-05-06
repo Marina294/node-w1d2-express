508b57b6e88e0c0969a26a95c1b11ffe22c2c1c0
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-05-06 04:13:37 -0700

    added 404 and admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const shopRouters = require('./routes/shop')
+const adminRouters = require('./routes/admin')
 
 const app = express()
 
@@ -25,39 +26,38 @@ const app = express()
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // })
 
+
 //to parse incoming form data --body parser
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/add-products', (req,res) => {
-    res.send(`
-        <form action="/product" method="POST">
-            <input type="text" name="title" />
-            <button type="submit">SEND</button>
-        </form>
-    `)
-})
+// app.use('/add-products', (req,res) => {
+//     res.send(`
+//         <form action="/product" method="POST">
+//             <input type="text" name="title" />
+//             <button type="submit">SEND</button>
+//         </form>
+//     `)
+// })
 
-app.use('/product', (req,res) => {
-    console.log(req.body)
-    res.redirect('/')
-})
+// app.use('/product', (req,res) => {
+//     console.log(req.body)
+//     res.redirect('/')
+// })
+
+// test.js を表示
+//serving files dynamically
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 // route
+app.use('/admin', adminRouters)
 app.use(shopRouters)
 
-//catch-all-middleware ------ 404
-
-app.get('/calculate',function(req,res){
-	res.send("Example GET");
-});
-
-app.use(function (req,res,next){
-	res.status(404).send('Unable to find the requested resource!');
-});
-
-app.listen(8080);
-
-
+// //catch-all-middleware ------ 404
+app.use((req,res,next) => {
+    res.status(404)
+    res.sendFile(path.join(__dirname, 'public','404.html'))
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req,res,next) => {
-    res.send('<h1>Hello from the other side</h1>')
+    res.send('<h1>Home page (with products)</h1>')
 })
 
 module.exports = router
